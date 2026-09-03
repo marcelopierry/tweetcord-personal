1a96138ba34d971e7c6a00f03b75a56bcab56c38
Marcelo Pierry <76548876+marcelopierry@users.noreply.github.com>
2026-09-02 20:51:19 -0700

    Add test for duplicate legacy users command removal
    Added a test to verify removal of duplicate legacy user command.

--- test/test_list_accounts.py
+++ test/test_list_accounts.py
@@ -1,3 +1,11 @@
+git: warning: confstr() failed with code 5: couldn't get path of DARWIN_USER_TEMP_DIR; using /tmp instead
+git: error: couldn't create cache file '/tmp/xcrun_db-YQAxNmwC' (errno=Operation not permitted)
+2026-09-02 22:51:13.637 xcodebuild[56286:5959616]  DVTFilePathFSEvents: Failed to start fs event stream.
+2026-09-02 22:51:13.764 xcodebuild[56286:5959615] [MT] DVTDeveloperPaths: Failed to get length of DARWIN_USER_CACHE_DIR from confstr(3), error = Error Domain=NSPOSIXErrorDomain Code=5 "Input/output error". Using NSCachesDirectory instead.
+git: warning: confstr() failed with code 5: couldn't get path of DARWIN_USER_TEMP_DIR; using /tmp instead
+git: error: couldn't create cache file '/tmp/xcrun_db-bfGOI5gH' (errno=Operation not permitted)
+2026-09-02 22:51:14.144 xcodebuild[56290:5959627]  DVTFilePathFSEvents: Failed to start fs event stream.
+2026-09-02 22:51:14.268 xcodebuild[56290:5959626] [MT] DVTDeveloperPaths: Failed to get length of DARWIN_USER_CACHE_DIR from confstr(3), error = Error Domain=NSPOSIXErrorDomain Code=5 "Input/output error". Using NSCachesDirectory instead.
 import asyncio
 import os
 import sqlite3
@@ -51,6 +59,10 @@ class TestListAccountsChannelFilter(unittest.TestCase):
     def test_channel_filter_cannot_escape_the_selected_server(self):
         usernames = asyncio.run(ListUsers._get_account_usernames(None, 123, '30'))
         self.assertEqual(usernames, [])
+
+    def test_duplicate_legacy_users_command_is_removed(self):
+        command_names = {command.name for command in ListUsers.list_group.commands}
+        self.assertEqual(command_names, {'accounts', 'settings'})
 
 
 if __name__ == '__main__':
