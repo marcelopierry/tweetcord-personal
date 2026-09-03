b3b36812514efebcd6324fcddb68e4af2ffe4387
Marcelo Pierry <76548876+marcelopierry@users.noreply.github.com>
2026-09-02 20:53:32 -0700

    Remove error logs from test_list_accounts.py

--- test/test_list_accounts.py
+++ test/test_list_accounts.py
@@ -1,11 +1,3 @@
-git: warning: confstr() failed with code 5: couldn't get path of DARWIN_USER_TEMP_DIR; using /tmp instead
-git: error: couldn't create cache file '/tmp/xcrun_db-YQAxNmwC' (errno=Operation not permitted)
-2026-09-02 22:51:13.637 xcodebuild[56286:5959616]  DVTFilePathFSEvents: Failed to start fs event stream.
-2026-09-02 22:51:13.764 xcodebuild[56286:5959615] [MT] DVTDeveloperPaths: Failed to get length of DARWIN_USER_CACHE_DIR from confstr(3), error = Error Domain=NSPOSIXErrorDomain Code=5 "Input/output error". Using NSCachesDirectory instead.
-git: warning: confstr() failed with code 5: couldn't get path of DARWIN_USER_TEMP_DIR; using /tmp instead
-git: error: couldn't create cache file '/tmp/xcrun_db-bfGOI5gH' (errno=Operation not permitted)
-2026-09-02 22:51:14.144 xcodebuild[56290:5959627]  DVTFilePathFSEvents: Failed to start fs event stream.
-2026-09-02 22:51:14.268 xcodebuild[56290:5959626] [MT] DVTDeveloperPaths: Failed to get length of DARWIN_USER_CACHE_DIR from confstr(3), error = Error Domain=NSPOSIXErrorDomain Code=5 "Input/output error". Using NSCachesDirectory instead.
 import asyncio
 import os
 import sqlite3
